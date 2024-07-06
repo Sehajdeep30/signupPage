8039a9be72d52c1d478ed94e06806ee2e7ffebe3
Sehajdeep30 <118628648+Sehajdeep30@users.noreply.github.com>
2024-07-07 03:42:52 +0530

    debuggin

--- server/index.js
+++ server/index.js
@@ -5,26 +5,47 @@ const UserModel = require("./models/Users");
 
 const app = express();
 app.use(express.json());
-app.use(cors(
-  {
-    origin: 'https://signup-page-brown.vercel.app', // Frontend URL
-    methods: ["POST","GET"],
-    credentials : true,
+app.use(
+  cors({
+    origin: "https://signup-page-brown.vercel.app", // Frontend URL
+    methods: ["POST", "GET"],
+    credentials: true,
     optionsSuccessStatus: 200,
-    allowedHeaders: ['Content-Type', 'Authorization']
-
-  }
-));
-mongoose.connect("mongodb+srv://Sehajdeep:rDhz7zUPr0@sehaj.lu7b4qy.mongodb.net/?retryWrites=true&w=majority&appName=Sehaj");
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
+mongoose.connect(
+  "mongodb+srv://Sehajdeep:rDhz7zUPr0@sehaj.lu7b4qy.mongodb.net/?retryWrites=true&w=majority&appName=Sehaj"
+);
 
 app.post("/register", (req, res) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+  ); // If needed
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "X-Requested-With,content-type"
+  ); // If needed
+  res.setHeader("Access-Control-Allow-Credentials", true);
   UserModel.create(req.body)
     .then((Users) => res.json(Users))
     .catch((err) => res.json(err));
 });
 
 app.post("/login", (req, res) => {
-  const {email, password} = req.body;
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+  ); // If needed
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "X-Requested-With,content-type"
+  ); // If needed
+  res.setHeader("Access-Control-Allow-Credentials", true);
+  const { email, password } = req.body;
   UserModel.findOne({ email: email })
     .then((user) => {
       if (user) {
@@ -33,8 +54,9 @@ app.post("/login", (req, res) => {
         } else {
           res.json("Incorrect Password");
         }
+      } else {
+        res.json("No record exists");
       }
-      else{res.json("No record exists")}
     })
     .catch((err) => console.log(err));
 });
