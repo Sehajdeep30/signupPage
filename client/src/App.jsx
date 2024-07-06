3f2bbed56131bfb913f79e8a3306e2316a6cd2af
Sehajdeep30 <118628648+Sehajdeep30@users.noreply.github.com>
2024-07-07 02:51:54 +0530

    work

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,21 +1,20 @@
 import "./App.css";
-import { BrowserRouter, Routes,Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import SignUp from "./SignUp";
 import Login from "./Login";
 import SignedUp from "./SignedUp";
-import Home from "./Home"
+import Home from "./Home";
 
 function App() {
-
-  return (  
+  return (
     <>
+      <Navigate to="/login" />
       <BrowserRouter>
         <Routes>
-          <Route path="/register" element={<SignUp/>}></Route>
-          <Route path="/login" element={<Login/>}></Route>
-          <Route path="/signedup" element={<SignedUp/>}></Route>
-          <Route path="/home" element={<Home/>}></Route>
-
+          <Route path="/register" element={<SignUp />}></Route>
+          <Route path="/login" element={<Login />}></Route>
+          <Route path="/signedup" element={<SignedUp />}></Route>
+          <Route path="/home" element={<Home />}></Route>
         </Routes>
       </BrowserRouter>
     </>
